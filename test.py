ba8fcf96faec178c583fd9a55bb481397b00605b
cefeng9988 <cfeng999@yahoo.com>
2024-04-10 20:04:41 -0400

    test.py

---
+++ test.py
@@ -0,0 +1,14 @@
+def handler(event, context):
+    return subtract(add(event['a'], event['b']), event['c'])
+
+def threeSum(self, nums: List[int]) -> List[List[int]]:
+    ans=set()
+    nums.sort()
+    n=len(nums)
+    for i in range(n-2):
+        for j in range(i+1,n-1):
+            for k in range(j+1,n):
+                temp=nums[i]+nums[j]+nums[k]
+                if temp==0:
+                    ans.add((nums[i],nums[j],nums[k]))
+    return ans
