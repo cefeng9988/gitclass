fa9212528b92f76daa157232d0423bc3043a512d
cefeng9988 <cfeng999@yahoo.com>
2024-04-10 20:08:00 -0400

    Adding Comments

--- test.py
+++ test.py
@@ -1,7 +1,27 @@
 def handler(event, context):
+    """
+    The function takes three input values, adds the first two, subtracts the third value from the
+    result, and returns the final result.
+    
+    :param event: The `event` parameter in the `handler` function is typically a dictionary or object
+    that contains information about the event that triggered the function. 
+    :param context: The `context` parameter in an AWS Lambda function represents the runtime information
+    of the Lambda function.
+    :return: The code is returning the result of subtracting the sum of 'a' and 'b' from 'c'.
+    """
     return subtract(add(event['a'], event['b']), event['c'])
 
 def threeSum(self, nums: List[int]) -> List[List[int]]:
+    """
+    The function `threeSum` takes a list of integers and returns a list of unique triplets where the sum
+    of the elements in each triplet is zero.
+    
+    :param nums: The `nums` parameter in the `threeSum` function is a list of integers. The function
+    aims to find all unique triplets in the list that sum up to zero. The function then returns a list
+    of lists containing these triplets
+    :type nums: List[int]
+    :return: A set of unique triplets of integers from the input list `nums` that sum up to zero.
+    """
     ans=set()
     nums.sort()
     n=len(nums)
